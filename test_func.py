869889192950792bf19d120899a0d09e2e1c85f2
Nadim El Helou <45805207+nadimelhelou@users.noreply.github.com>
2020-02-28 16:18:33 -0500

    Add test cases

---
+++ test_func.py
@@ -0,0 +1,29 @@
+from PIL import Image, ImageFont, ImageDraw
+import os
+from video import *
+
+# Test the delete_images() function
+def test_delete_images():
+	img = Image.new('RGB', (600, 400), color=(65,5,184))
+	img.save("image1.png")
+	delete_images("image")
+
+	flag = True
+	for image in os.listdir('.'):
+		if image.startswith("image") & image.endswith('.png'):
+			flag = False
+
+	assert flag == True
+
+
+# Test the delete_video() function
+def test_delete_video():
+	delete_video("nadimelhelou")
+
+	flag = True
+	for video in os.listdir('.'):
+		if video.startswith("daily_tweets_nadimelhelou") & video.endswith('.mp4'):
+			flag = False
+
+	assert flag == True
+    
